c3fe80f6da44e23e43642c8b2c4c9916cd283102
Daniel Luis <dan.dluis.dl@gmail.com>
2026-02-16 16:08:11 -0300

    feat: Refactor development settings to use environment variables for security and CORS configurations

--- ler_o_brasil/settings/dev.py
+++ ler_o_brasil/settings/dev.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 
 from .base import *
+import os
 
 # Load environment variables from .env file in development
 load_dotenv()
@@ -9,30 +10,23 @@ load_dotenv()
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i#81c2yqf^^knez7$j_8#*d00@vew9iaxvek+_39bu(^i-ppd9"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-i#81c2yqf^^knez7$j_8#*d00@vew9iaxvek+_39bu(^i-ppd9")
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # CORS
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
-    "http://localhost:8081",
-    "http://127.0.0.1:8081",
-]
+CORS_ALLOWED_ORIGINS = os.getenv("DJANGO_CORS_ALLOWED_ORIGINS", "").split(",")
 
-CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:8080",
-    "http://localhost:8081",
-    "http://127.0.0.1:8081",
-]
+CORS_ALLOW_CREDENTIALS = os.getenv("DJANGO_CORS_ALLOW_CREDENTIALS", "true").lower() in ("true", "1", "t")
+
+CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(",")
 
 try:
     from .local import *
 except ImportError:
     pass
+
