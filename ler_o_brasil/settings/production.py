5e9a3b7844f701940c7fa702464256ea849a7a06
Dan-Source <dan.dluis.dl@gmail.com>
2026-06-08 20:36:03 -0300

    feat: Add logging configuration for production settings

--- ler_o_brasil/settings/production.py
+++ ler_o_brasil/settings/production.py
@@ -73,6 +73,20 @@ STORAGES["staticfiles"]["BACKEND"] = (
     "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 )
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
+
 
 try:
     from .local import *
